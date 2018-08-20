3c90659bdb16fd72fe5e05e196adaec03a44803d
Mayur Patil (मयूर पाटील) <ram.nath241089@gmail.com>
2018-08-20 19:13:53 +0530

    Update Challenge-#1.py

--- Easy/Challenge-#1.py
+++ Easy/Challenge-#1.py
@@ -1,1 +1,8 @@
+#create a program that will ask the users name, age, and reddit username. have it tell them the information back, in the format:
+#your name is (blank), you are (blank) years old, and your username is (blank)
 
+name = int(input("Enter name: "))
+age = int(input("Enter age: "))
+uname = int(input("Enter username: "))
+
+print("your name is {}, you are {} years old, and your username is {}".format(name,age,uname))
